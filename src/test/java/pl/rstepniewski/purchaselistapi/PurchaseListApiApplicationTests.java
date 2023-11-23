42d92a5a69046b32a7b481c174b2ad5a982e43b2
Rafal Stepniewski <vultur12@gmail.com>
2023-11-23 16:10:11 +0100

    Initial commit

---
+++ src/test/java/pl/rstepniewski/purchaselistapi/PurchaseListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rstepniewski.purchaselistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
