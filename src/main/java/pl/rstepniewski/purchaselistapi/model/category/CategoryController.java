88e82d6d121abb6c71a021978c6843c56bd9b739
Rafal Stepniewski <vultur12@gmail.com>
2023-12-12 22:12:58 +0100

    Category is DTO and entity now. New and working Category endpoint. registered_user table insted of _user table

---
+++ src/main/java/pl/rstepniewski/purchaselistapi/model/category/CategoryController.java
@@ -0,0 +1,23 @@
+package pl.rstepniewski.purchaselistapi.model.category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> sayHello() {
+        return ResponseEntity.ok(categoryRepository.findAll());
+    }
+
+}
